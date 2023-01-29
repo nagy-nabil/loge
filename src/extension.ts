894209c0b54aec39d67e544b89d72302188301c6
nagy nabil <nagynabil651@gmail.com>
2023-01-29 02:37:17 +0200

    barebone login - only work with rust/js/ts - insert the log line in the next line(need some changes) - add shortcut for it ctrl+alt+l - the token is the word under the cursor - TODO is there's selection use only the selection instead of the complete word

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,47 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "loge" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('loge.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from loge!');
-	});
-
-	context.subscriptions.push(disposable);
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand(
+        "loge.loge",
+        () => {
+            // The code you place here will be executed every time your command is executed
+            let editor = vscode.window.activeTextEditor;
+            // if no editor opened do nothing
+            if (!editor) {
+                return;
+            }
+            let position = editor.selections[0].active;
+            const newPos = position.with(position.line + 1, 0);
+            let document = editor.document;
+            const langId = document.languageId;
+            // if the lang not js or ts or rust do nothing more
+            if (
+                langId !== "javascript" &&
+                langId !== "typescript" &&
+                langId !== "rust"
+            ) {
+                return;
+            }
+            let range = document.getWordRangeAtPosition(position);
+            let token = document.getText(range);
+            // edit the document based on which lang is running
+            editor?.edit((editor) => {
+                const newInsert =
+                    langId === "rust"
+                        ? `println!("🪵 line \\"${position.line+1}\\" ~ token ~ ${token} {${token}}");`
+                        : `console.log("🪵 line \\"${position.line+1}\\" ~ token ~ ${token}", ${token});`;
+                editor.insert(newPos, `\n\t${newInsert}\n`);
+            });
+        }
+    );
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
