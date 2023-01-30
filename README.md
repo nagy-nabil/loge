cb82285276af56ed58c6694045a0202cee3c87b0
nagy nabil <nagynabil651@gmail.com>
2023-01-31 01:49:41 +0200

    selection with higher priority

--- README.md
+++ README.md
@@ -1,71 +1,31 @@
-# loge README
+# LOGE
 
-This is the README for your extension "loge". After writing up a brief description, we recommend including the following sections.
+LOGE is an extension to make repeted logging for js/ts/rust(console.log, println!)  just a little bit easier
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+- how to use it
+    - from the command palette under the name `Loge`
+    - the default shortcut `ctrl+alt+l`
 
-For example if there is an image subfolder under your extension project workspace:
+<br />
 
-\!\[feature X\]\(images/feature-x.png\)
+1. log the selected text under the cursor
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+    ![log the selected text](/images/selected.gif)
 
-## Requirements
+2. log the word under the cursor
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+    ![log the word under the cursor](/images/unselected.gif)
 
-## Extension Settings
+<br />
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+> Note that always selection has higher priority than the word under the cursor
 
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
 
-### 1.0.0
+### 0.0.1
 
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+intial relase for loge
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import path = require("path");
 import * as vscode from "vscode";
 
 // This method is called when your extension is activated
@@ -8,39 +9,58 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand(
-        "loge.loge",
-        () => {
-            // The code you place here will be executed every time your command is executed
-            let editor = vscode.window.activeTextEditor;
-            // if no editor opened do nothing
-            if (!editor) {
-                return;
-            }
-            let position = editor.selections[0].active;
-            const newPos = position.with(position.line + 1, 0);
-            let document = editor.document;
-            const langId = document.languageId;
-            // if the lang not js or ts or rust do nothing more
-            if (
-                langId !== "javascript" &&
-                langId !== "typescript" &&
-                langId !== "rust"
-            ) {
-                return;
-            }
-            let range = document.getWordRangeAtPosition(position);
-            let token = document.getText(range);
-            // edit the document based on which lang is running
-            editor?.edit((editor) => {
-                const newInsert =
-                    langId === "rust"
-                        ? `println!("🪵 line \\"${position.line+1}\\" ~ token ~ ${token} {${token}}");`
-                        : `console.log("🪵 line \\"${position.line+1}\\" ~ token ~ ${token}", ${token});`;
-                editor.insert(newPos, `\n\t${newInsert}\n`);
-            });
+    let disposable = vscode.commands.registerCommand("loge.loge", async () => {
+        // The code you place here will be executed every time your command is executed
+        let editor = vscode.window.activeTextEditor;
+        // if no editor opened do nothing
+        if (!editor) {
+            return;
         }
-    );
+        let position = editor.selection.active;
+        // position where selection start
+        const anchor = editor.selection.anchor;
+        let document = editor.document;
+        const langId = document.languageId;
+        // if the lang not js or ts or rust do nothing more
+        if (
+            langId !== "javascript" &&
+            langId !== "typescript" &&
+            langId !== "rust"
+        ) {
+            return;
+        }
+        // if the anchor in not the same as cursor position means there's selection and selection has high priority in the loggin, else the extension will try to get the current word under the cursor
+        let range: vscode.Range | undefined;
+        if (anchor.isEqual(position)) {
+            range = document.getWordRangeAtPosition(position);
+        } else {
+            range = new vscode.Range(position, anchor);
+        }
+        // only get the token if there's a range else return and do nothing better than trying to print to whole document
+        let token: string;
+        if (range !== undefined) {
+            token = document.getText(range);
+        } else {
+            return ;
+        }
+        // use built in command to add new line down just easir than managing the new line myself
+        await vscode.commands.executeCommand("editor.action.insertLineAfter");
+        // position after inserting new line
+        const insertPos = editor.selection.active;
+        // edit the document based on which lang is running
+        editor?.edit((editor) => {
+            const fName = path.basename(document.fileName);
+            const newInsert =
+                langId === "rust"
+                    ? `println!("🪵 file \\"${fName}\\" ~ line \\"${
+                        position.line + 1
+                    }\\" ~ token ~ ${token} = {${token}}");`
+                    : `console.log("🪵 file \\"${fName}\\" ~  line \\"${
+                        position.line + 1
+                    }\\" ~ token ~ ${token} = ", ${token});`;
+            editor.insert(insertPos, `${newInsert}`);
+        });
+    });
     context.subscriptions.push(disposable);
 }
 
